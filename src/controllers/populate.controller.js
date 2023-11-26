b6e682ee15c93631c38c976c27c3f95f5631c46f
kritik <kritikverma2002@gmail.com>
2023-11-26 18:49:59 +0530

    populate apis added

--- src/controllers/populate.controller.js
+++ src/controllers/populate.controller.js
@@ -4,11 +4,11 @@ const staticTaskData = require('../data/task.json');
 const User = require('../models/user.model');
 const Task = require('../models/task.model');
 
-exports.pushUsers = async (req, res, next) => {
+exports.populateDataBase = async (req, res, next) => {
     try {
         await User.deleteMany({});
         await Task.deleteMany({});
-        
+
         const insertedUsers = await User.insertMany(staticUserData, {
             validateBeforeSave: false,
         });
@@ -24,6 +24,56 @@ exports.pushUsers = async (req, res, next) => {
             },
             data: {
                 users: insertedUsers,
+                tasks: insertedTasks,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: error.message,
+        });
+    }
+};
+
+exports.populateUsers = async (req, res, next) => {
+    try {
+        await User.deleteMany({});
+
+        const insertedUsers = await User.insertMany(staticUserData, {
+            validateBeforeSave: false,
+        });
+
+        res.status(201).json({
+            status: 'success',
+            metadata: {
+                users_size: insertedUsers.length,
+            },
+            data: {
+                users: insertedUsers,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: error.message,
+        });
+    }
+};
+
+exports.populateTasks = async (req, res, next) => {
+    try {
+        await Task.deleteMany({});
+
+        const insertedTasks = await Task.insertMany(staticTaskData, {
+            validateBeforeSave: false,
+        });
+
+        res.status(201).json({
+            status: 'success',
+            metadata: {
+                tasks_size: insertedTasks.length,
+            },
+            data: {
                 tasks: insertedTasks,
             },
         });
--- src/routes/populate.routes.js
+++ src/routes/populate.routes.js
@@ -3,6 +3,8 @@ const populateController  = require("../controllers/populate.controller");
 
 const router = express.Router();
 
-router.route("/").get(populateController.pushUsers);
+router.route("/").get(populateController.populateDataBase);
+router.route("/users").get(populateController.populateUsers);
+router.route("/tasks").get(populateController.populateTasks);
 
 module.exports = router;
